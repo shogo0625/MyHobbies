8146c1ae7b3d405647e811c3a6dbc2ce74e7567b
shogo0625 <shogo19940625@gmail.com>
2020-09-03 13:53:39 +0900

    Create hobbyTagController and filtered Hobbies index

--- app/Tag.php
+++ app/Tag.php
@@ -11,6 +11,13 @@ class Tag extends Model
         return $this->belongsToMany('App\Hobby'); // 中間テーブル
     }
 
+    public function filteredHobbies()
+    {
+        return $this->belongsToMany('App\Hobby')
+            ->wherePivot('tag_id', $this->id)
+            ->orderBy('updated_at', 'DESC');
+    }
+
     /**
      * The attributes that are mass assignable.
      *
--- resources/views/hobby/index.blade.php
+++ resources/views/hobby/index.blade.php
@@ -5,7 +5,15 @@
     <div class="row justify-content-center">
         <div class="col-md-11">
             <div class="card">
+
+                @isset($filter)
+                <div class="card-header">Filtered hobbies by
+                    <span style="font-size:130%;" class="badge badge-{{ $filter->style }}">{{ $filter->name }}</span>
+                    <span class="float-right"><a href="/hobby">Show all Hobbies</a></span>
+                </div>
+                @else
                 <div class="card-header">All the hobbies</div>
+                @endisset
 
                 <div class="card-body">
                     <ul class="list-group">
@@ -27,7 +35,7 @@
                             <span class="float-right mx-3">{{ $hobby->created_at->diffForHumans() }}</span>
                             <br>
                             @foreach($hobby->tags as $tag)
-                            <a href=""><span class="badge badge-{{ $tag->style }}">{{ $tag->name }}</span></a>
+                            <a href="/hobby/tag/{{ $tag->id }}"><span class="badge badge-{{ $tag->style }}">{{ $tag->name }}</span></a>
                             @endforeach
                         </li>
                         @endforeach
