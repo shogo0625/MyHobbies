22dca1069c9a8b57c797314ec4d1b01fc0248a3b
shogo0625 <shogo19940625@gmail.com>
2020-09-04 19:20:49 +0900

    Prepare frontend for images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,37 +5,51 @@
     <div class="row justify-content-center">
         <div class="col-md-11">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
-                    <h2>Hello {{ auth()->user()->name }}</h2>
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
+                    <div class="row">
+                        <div class="col-md-9">
+                            <h2>Hello {{ auth()->user()->name }}</h2>
+                            <h5>Your Motto</h5>
+                            <p>
+                                <p>{{ auth()->user()->motto ?? '' }}</p>
+                            </p>
+                            <h5>Your "About Me" -Text</h5>
+                            <p>
+                                <p>{{ auth()->user()->about_me ?? '' }}</p>
+                            </p>
+                        </div>
+                        <div class="col-md-3">
+                            <img class="img-thumbnail" src="/img/300x400.jpg" alt="{{ auth()->user()->name }}">
+                        </div>
                     </div>
-                    @endif
+
+
 
                     @isset($hobbies)
                     @if($hobbies->count() > 0)
                     <h3>Your Hobbies:</h3>
                     @endif
                     <ul class="list-group">
-                        @foreach ($hobbies as $hobby)
+                        @foreach($hobbies as $hobby)
                         <li class="list-group-item">
-                            <a title="Show Details" href="/hobby/{{ $hobby->id }}">{{ $hobby->name }}</a>
+                            <a title="Show Details" href="/hobby/{{ $hobby->id }}">
+                                <img src="/img/thumb_landscape.jpg" alt="thumb"></a>
+                            {{ $hobby->name }}
+                            </a>
                             @auth
                             <a class="btn btn-sm btn-light ml-2" href="/hobby/{{ $hobby->id }}/edit"><i class="fas fa-edit"></i> Edit Hobby</a>
                             @endauth
 
                             @auth
-                            <form class="float-right" style="display:inline" action="/hobby/{{ $hobby->id }}" method="post">
+                            <form class="float-right" style="display: inline" action="/hobby/{{ $hobby->id }}" method="post">
                                 @csrf
                                 @method("DELETE")
                                 <input class="btn btn-sm btn-outline-danger" type="submit" value="Delete">
                             </form>
                             @endauth
-                            <!-- diffForHumansはCarbonライブラリのメソッド -->
-                            <span class="float-right mx-3">{{ $hobby->created_at->diffForHumans() }}</span>
+                            <span class="float-right mx-2">{{ $hobby->created_at->diffForHumans() }}</span>
                             <br>
                             @foreach($hobby->tags as $tag)
                             <a href="/hobby/tag/{{ $tag->id }}"><span class="badge badge-{{ $tag->style }}">{{ $tag->name }}</span></a>
@@ -45,7 +59,7 @@
                     </ul>
                     @endisset
 
-                    <a href="/hobby/create" class="btn btn-success btn-sm"><i class="fas fa-plus-circle"></i> Create new Hobby</a>
+                    <a class="btn btn-success btn-sm" href="/hobby/create"><i class="fas fa-plus-circle"></i> Create new Hobby</a>
                 </div>
             </div>
         </div>
